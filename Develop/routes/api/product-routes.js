bca36bfa2822973f5126f3af18803e520cca9612
mlukicdesign <mlukicdesign@gmail.com>
2023-06-22 18:31:48 +0800

    progress upload

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -40,6 +40,8 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
+
+    
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
