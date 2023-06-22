ea6f0495ca53bf987fe6ab515aece85f0d9e38be
mlukicdesign <mlukicdesign@gmail.com>
2023-06-22 14:24:19 +0800

    server erros

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -12,15 +12,4 @@ const sequelize = process.env.JAWSDB_URL
       },
     });
 
-// const sequelize = new Sequelize(
-//   process.env.DB_NAME,
-//   process.env.DB_USER,
-//   process.env.DB_PW, {
-//       host: 'localhost',
-//       dialect: 'mysql',
-//       dialectOptions: {
-//         decimalNumbers: true,
-//       },
-//     });
-
 module.exports = sequelize;
--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
+// import sequelize connection
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,13 +12,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-
 sequelize.sync().then(() => {
-  app.listen(PORT, () => console.log(`App listing on port ${PORT}`));
+  app.listen(PORT, () => console.log('Now listening'));
 });
 
 
-// app.listen(PORT, () => {
-//   console.log(`App listening on port ${PORT}!`);
-// });
 
